13b9dd9ff1793c2106cee7fd1cbf685dd44b1b7e
Sam <barrett370@gmail.com>
2019-10-01 16:14:05 +0100

    revert test

--- foo/tests/test_main.py
+++ foo/tests/test_main.py
@@ -4,7 +4,7 @@ from foo.main import *
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(False, to_test())
+        self.assertEqual(True, to_test())
 
 
 if __name__ == '__main__':
