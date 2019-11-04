e5b51168c17634659f4ce498acf30e973b18b91e
Sam <barrett370@gmail.com>
2019-11-04 10:00:13 +0000

    rework turning code

--- catkinws/src/auto_motion/motionlaser.py
+++ catkinws/src/auto_motion/motionlaser.py
@@ -50,7 +50,9 @@ else:
     # Optimal Ranges
     RIGHT_OPTIMAL = 0.5
     RIGHT_MAX = 2.0
-    FRONT_MIN = 3.0
+    RIGHT_MIN = 1.0
+    FRONT_MIN = 2.0
+    LEFT_MIN = 1.0
 
 
 def clean_laser_readings(msg):
@@ -98,90 +100,12 @@ def strip_nan(some_readings, value):
     some_readings.append(val)
 
 
-# def callback(msg):
-#     global turn
-#     global flip
-#     global sum_readings
-#     global desired_bearing
-#     global average_count
-#     laser_val = msg.ranges[::-1]
-#     average_count +=1
-#     print(len(sum_readings))
-#     if average_count % RATE == 0:
-#         # print(average_count)
-#         mapped_readings = map(lambda x: x / RATE, sum_readings)
-
-#         # fig.canvas.draw()
-#         sum_readings = []
-#         for value in laser_val:
-#             strip_nan(sum_readings, value)
-
-#         left = mapped_readings[left_lower:left_upper]
-#         centre_left = mapped_readings[centre_left_lower: centre_left_upper]
-#         centre = mapped_readings[centre_lower: centre_upper]
-#         centre_right = mapped_readings[centre_right_lower:centre_right_upper]
-#         right = mapped_readings[right_lower:right_upper]
-#         # print(left)
-#         left_avg = reduce(lambda a, b: a + b, left) / len(left)
-#         centre_left_avg = reduce(lambda a, b: a + b, centre_left) / len(centre_left)
-#         centre_avg = reduce(lambda a, b: a + b, centre) / len(centre)
-#         centre_right_avg = reduce(lambda a, b: a + b, centre_right) / len(centre_right)
-#         right_avg = reduce(lambda a, b: a + b, right) / len(right)
-#         print(left_avg,centre_avg,right_avg)
-        
-#         # print(random.uniform(0, 1))
-#         # print msg.ranges[250]
-#         # check centre and right
-#         if centre_avg < 1.1:  # turn
-#             if right_avg > 1.7:
-#                 desired_bearing = RIGHT
-#                 print("turning right 1")
-#                 turn = True
-#                 # turn right
-#             elif left_avg > 1.7:
-#                 desired_bearing = LEFT
-#                 print("turning left")
-#                 turn = True
-#                 # turn left
-#             else:
-#                 desired_bearing = BACKWARDS
-#                 print("reversing, beep beep beep")
-#                 turn = True
-#                 # reverse and recurse
-#         elif centre_avg > 1.1 and right_avg > 4.5 and left_avg > 4.5:
-#             desired_bearing = FORWARD
-#             turn = False
-#             print("Cant find anything, heading forward")
-#         elif centre_avg > 1.1 and right_avg > 1.7:  # space to the right
-#             desired_bearing = RIGHT
-#             print("turning right 2 ")
-#             turn = True
-#             # turn right
-#         elif right_avg < 0.6 < left_avg:
-#             # turn left
-#             desired_bearing = LEFT
-#             turn = True
-#         elif left_avg < 0.6 < right_avg:
-#             desired_bearing = RIGHT
-#             turn = True
-#         else:
-#             desired_bearing = FORWARD
-#             print("keeping on")
-#             turn = False
-
-
-#     else:
-#         temp_values = []
-#         for value in msg.ranges:
-#             strip_nan(temp_values, value)
-
-#         sum_readings = [x + y for x, y in zip(temp_values, sum_readings)]
-
-
 def callback(msg):
     global average_count
     global RATE
     global sum_readings
+    global turn
+    global desired_bearing
     if average_count % RATE == 0:
         # plt.clf()
         mapped_readings = map(lambda x: x / RATE, sum_readings)
@@ -197,6 +121,7 @@ def callback(msg):
         centre_right = mapped_readings[CENTRE_RIGHT_LOWER:CENTRE_RIGHT_UPPER]
         right = mapped_readings[RIGHT_LOWER:RIGHT_UPPER]
 
+        # CALCUATE AVERAGE READINGS
         left_avg = reduce(lambda a, b: a + b, left) / len(left)
         centre_left_avg = reduce(lambda a, b: a + b, centre_left) / len(centre_left)
         centre_avg = reduce(lambda a, b: a + b, centre) / len(centre)
@@ -204,17 +129,8 @@ def callback(msg):
         right_avg = reduce(lambda a, b: a + b, right) / len(right)
         print(left_avg,centre_avg,right_avg)
         avg_data = []
+        # pad advverages for graphing
         for i in range(len(mapped_readings)):
-            # if i < left_lower:
-            #     average_data.append(0)
-            # elif i == left_lower:
-            #     for j in range(len(left)):
-            #         average_data.append(left_avg)
-            #     i = left_upper
-            # elif i < centre_lower:
-            #     average_data.append(0)
-            # elif i == centre_lower:
-            #     for j in range(len(centre)):
             if (i < LEFT_LOWER):
                 avg_data.append(0)
             elif (i < LEFT_UPPER):
@@ -228,45 +144,24 @@ def callback(msg):
             elif (i < RIGHT_UPPER):
                 avg_data.append(right_avg)
 
-        if centre_avg < FRONT_MIN:  # turn
-            if right_avg > RIGHT_MAX:
+        # SPACE FORWARD?
+        if centre_avg >= FRONT_MIN: # turn
+            # SPACE RIGHT?
+            if right_avg >= RIGHT_MIN:
+                # TURN RIGHT
+                turn = True
                 desired_bearing = RIGHT
-                print("turning right, \n too little space front, enough space right")
+            else: # PIVOT
                 turn = True
-                # turn right
-            elif left_avg > 1.7:
                 desired_bearing = LEFT
-                print("turning left, no space front or right")
-                turn = True
-                # turn left
-            else:
-                desired_bearing = BACKWARDS
-                print("reversing, beep beep beep")
-                turn = True
-                # reverse and recurse
-        elif centre_avg > FRONT_MIN and right_avg == RIGHT_OPTIMAL and left_avg > 4.5:
-            desired_bearing = FORWARD
-            turn = False
-            print("Cant find anything, heading forward")
-        elif centre_avg > 1.1 and right_avg > 1.7:  # space to the right
-            desired_bearing = RIGHT
-            print("turning right 2 ")
-            turn = True
-            # turn right
-        elif right_avg < 0.6 < left_avg:
-            # turn left
-            desired_bearing = LEFT
-            turn = True
-        elif left_avg < 0.6 < right_avg:
-            desired_bearing = RIGHT
-            turn = True
         else:
-            desired_bearing = FORWARD
-            print("keeping on")
-            turn = False
-
-
-
+            # SPACE RIGHT?
+            if right_avg >= RIGHT_MIN:
+                turn = True
+                desired_bearing = RIGHT
+            else:
+                turn = False
+                desired_bearing = FORWARD
         # plt.plot(avg_data)
         # fig.canvas.draw()
     else:
