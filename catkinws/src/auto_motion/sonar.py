7fdd41850a6c59ed57299d987c82ab91454fdac6
Charles de Freitas <charles.defreitas@progressiveaccess.com>
2019-11-05 10:45:16 +0000

    Add test movment

--- catkinws/src/auto_motion/sonar.py
+++ catkinws/src/auto_motion/sonar.py
@@ -3,15 +3,26 @@ from pprint import pprint
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
-#p2os_msgs/SonarArray
 from p2os_msgs.msg import SonarArray
 
-def callback(msg):
-    pprint(msg.ranges)
-rospy.init_node("sonar", anonymous=False)
-sub = rospy.Subscriber('/sonar', SonarArray, callback)
-rate = rospy.Rate(10)
-while not rospy.is_shutdown():
-    rate.sleep()
 
+class Sonar:
+
+    def __init__(self):
+        rospy.init_node("sonar", anonymous=False)
+        self.sub = rospy.Subscriber('/sonar', SonarArray, self.callback)
+        self.pub = rospy.Publisher('/cmd_vel', Twist)
+        rate = rospy.Rate(10)
+        self.cmd = Twist()
+        while not rospy.is_shutdown():
+            if len(list(filter(lambda x: x<0.5,self.ranges)))>0:
+                self.cmd.linear.x = 0
+            else:
+                self.cmd.linear.x = 0.5
+
+            rate.sleep()
+
+    def callback(self, msg):
+        pprint(msg.ranges)
+        self.ranges = msg.ranges
 
