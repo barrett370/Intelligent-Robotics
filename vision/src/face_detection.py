2e97fb13d35df5f0ace2ef3f59e4e1be84954e9e
Sam <barrett370@gmail.com>
2019-11-15 14:30:27 +0000

    add method for counting faces

--- vision/src/face_detection.py
+++ vision/src/face_detection.py
@@ -3,12 +3,12 @@ import cv2
 
 class FaceDetector:
 
-    def __init__(self, camera: int, classifier_path: str):
+    def __init__(self, camera: int, classifier_path: str) -> None:
         self.face_cascade = cv2.CascadeClassifier(classifier_path)
         # To capture video from webcam.
-        self.cap = cv2.VideoCapture(2)
+        self.cap = cv2.VideoCapture(camera)
 
-    def detect_faces(self):
+    def cont_detect_faces(self) -> None:
         while True:
             # Read the frame
             _, img = self.cap.read()
@@ -16,7 +16,6 @@ class FaceDetector:
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             # Detect the faces
             faces = self.face_cascade.detectMultiScale(gray, 1.1, 4)
-            print(len(faces))
             # Draw the rectangle around each face
             for (x, y, w, h) in faces:
                 cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
@@ -26,4 +25,12 @@ class FaceDetector:
             k = cv2.waitKey(30) & 0xff
             if k == 27:
                 self.cap.release()
-                
+
+    def count_faces(self) -> int:
+        _, img = self.cap.read()
+        # Convert to grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # Detect the faces
+        faces = self.face_cascade.detectMultiScale(gray, 1.1, 4)
+
+        return len(faces)
