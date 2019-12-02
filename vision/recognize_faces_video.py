7efa811ef74fd6321061c352d99dd849eccae91e
george welly <cyberhylian@gmail.com>
2019-12-02 15:25:54 +0000

    tried to find count flag for top face

--- vision/recognize_faces_video.py
+++ vision/recognize_faces_video.py
@@ -79,6 +79,7 @@ while True:
             # of votes (note: in the event of an unlikely tie Python
             # will select first entry in the dictionary)
             name = max(counts, key=counts.get)
+            print("max counts:" + max(counts, key=counts.get))
 
         # update the list of names
         names.append(name)
@@ -97,7 +98,7 @@ while True:
         y = top - 15 if top - 15 > 15 else top + 15
         cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
                     0.75, (0, 255, 0), 2)
-        print(name)
+        # print(name)
 
     # if the video writer is None *AND* we are supposed to write
     # the output video to disk initialize the writer
