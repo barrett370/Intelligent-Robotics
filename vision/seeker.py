7444dbf5236e104ab824622d685eaffbbe7437d9
george welly <cyberhylian@gmail.com>
2019-12-03 13:37:59 +0000

    removing unnecessary comments, constants and other code

--- vision/seeker.py
+++ vision/seeker.py
@@ -11,10 +11,6 @@ import cv2
 # Movement code
 CONFIDENT_GUESSES_THRESHOLD = 3
 ACTIVATION_THRESHOLD = 20
-RIGHT = -90
-LEFT = 90
-BACKWARDS = 180
-FORWARD = 0
 HZ = 10
 confidentGuesses = 0
 
@@ -25,8 +21,6 @@ print('initialised node to mover')
 rate = rospy.Rate(HZ)  # 10hz
 base_data = Twist()
 found = False
-# Movement code
-
 
 # construct the argument parser and parse the arguments
 argParser = argparse.ArgumentParser()
@@ -63,7 +57,6 @@ while not found:
     # a width of 750px (to speedup processing)
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     rgb = imutils.resize(frame, height=240, width=426)
-    # r = frame.shape[1] / float(rgb.shape[1])
 
     # detect the (x, y)-coordinates of the bounding boxes
     # corresponding to each face in the input frame, then compute
@@ -86,18 +79,13 @@ while not found:
             # find the indexes of all matched faces then initialize a
             # dictionary to count the total number of times each face
             # was matched
-            matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+            faceMatches = [i for (i, b) in enumerate(matches) if b]
             counts = {}
-
             # loop over the matched indexes and maintain a count for
             # each recognized face face
-            for i in matchedIdxs:
+            for i in faceMatches:
                 name = data["names"][i]
                 counts[name] = counts.get(name, 0) + 1
-
-            # determine the recognized face with the largest number
-            # of votes (note: in the event of an unlikely tie Python
-            # will select first entry in the dictionary)
             name = max(counts, key=counts.get)
             matchCount = counts[name]
             # sumCounts = sumCounts + matchCount
@@ -111,52 +99,10 @@ while not found:
         # update the list of names
         names.append(name)
 
-    # loop over the recognized faces
-    # for ((top, right, bottom, left), name) in zip(boxes, names):
-    #     # rescale the face coordinates
-    #     top = int(top * r)
-    #     right = int(right * r)
-    #     bottom = int(bottom * r)
-    #     left = int(left * r)
-
-        # draw the predicted face name on the image
-        # cv2.rectangle(frame, (left, top), (right, bottom),
-        #               (0, 255, 0), 2)
-        # y = top - 15 if top - 15 > 15 else top + 15
-        # cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-        #             0.75, (0, 255, 0), 2)
-        # print(name)
-
-    # if the video writer is None *AND* we are supposed to write
-    # the output video to disk initialize the writer
-    # if writer is None and args["output"] is not None:
-    #     fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-    #     writer = cv2.VideoWriter(args["output"], fourcc, 20,
-    #                              (frame.shape[1], frame.shape[0]), True)
-    #
-    # # if the writer is not None, write the frame with recognized
-    # # faces t odisk
-    # if writer is not None:
-    #     writer.write(frame)
-
-    # check to see if we are supposed to display the output frame to
-    # the screen
-    # if args["display"] > 0:
-    #     cv2.imshow("Frame", frame)
-    #     key = cv2.waitKey(1) & 0xFF
-
-        # if the `q` key was pressed, break from the loop
-        # key = cv2.waitKey(1) & 0xFF
-        # if key == ord("q"):
-        #     break
-
 # do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
 
-# # check to see if the video writer point needs to be released
-# if writer is not None:
-#     writer.release()
 
 
 
