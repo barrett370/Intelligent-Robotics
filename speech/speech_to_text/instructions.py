5c39ddb54fbdc306f5dc06de3a65eb2e94a1a525
Sam Barrett <barrett370@gmail.com>
2019-12-04 16:02:49 +0000

    fix

--- speech/speech_to_text/instructions.py
+++ speech/speech_to_text/instructions.py
@@ -2,8 +2,10 @@ import os
 
 import requests
 
-from misc_functions import strip_leading_space
-import utils.names as namess
+from speech_to_text.misc_functions import strip_leading_space
+
+import utils.names as names
+
 language = 'en'
 
 
@@ -21,19 +23,20 @@ def strip_dets(instruction: str) -> str:
         instruction = instruction.replace(det, "")
     return instruction
 
+
 def get_loc():
     print("get_loc")
     req = requests.get("http://localhost:5000/getRelLoc")
     print(req.json())
     requests.get(f"http://localhost:5001/say/{req.json()['text']}")
+
+
 class InstructionParser:
     instructions = {
         "print something": lambda: print("Printed Something"),
         "what is your name": lambda: print("My name is Howard!"),
-        "where am i" : get_loc
+        "where am i": get_loc
     }
-
-
 
     def parse(self, instruction: str) -> bool:
         """         
@@ -64,7 +67,7 @@ class InstructionParser:
             return True
             # send to landmarks API
         elif instruction.__contains__("find"):
-            name - strip_leading_space(instruction.split("find")[1])
+            name = strip_leading_space(instruction.split("find")[1])
             print(f"Finding {names.get_id(name)}")
             requests.get(f"http://localhost:5000/say/Finding {names.get_id(name)}")
             requests.get(f"http://localhost:5000/seek/{names.get_id(name)}")
