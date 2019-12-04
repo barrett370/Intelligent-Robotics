c680b9e840a930e617e2a340155172b284842d21
Sam Barrett <barrett370@gmail.com>
2019-12-04 16:28:55 +0000

    entropy for names

--- speech/speech_to_text/names.py
+++ speech/speech_to_text/names.py
@@ -1,4 +1,6 @@
 
+import difflib
+import requests
 
 names = {
     "anant": 0 ,
@@ -9,5 +11,18 @@ names = {
 }
 
 
-def get_id(name:str)->int:
-    return names[name]
+def get_id(name_input:str)->int:
+    max_sim = 0
+    sim_name = ""
+    for name in list(names.keys()):
+        sim = difflib.SequenceMatcher(a= name, b=name_input  .lower()).ratio()
+        if sim > max_sim:
+            max_sim = sim
+            sim_name = name     
+    print(f"Max sim: {max_sim}, most similar name: {sim_name}")
+    if max_sim > 0.5:
+        return names[sim_name]
+    else:
+        string = "I don't know anyone by that name"
+        requests.get(f"http://localhost:5001/say/{string}")
+        return
