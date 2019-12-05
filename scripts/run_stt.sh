718bdb4898eaffcabbcc92264f8d369ea36dc433
Charles de Freitas <charles@defreitas.io>
2019-12-05 12:49:40 +0000

    .

--- scripts/run_stt.sh
+++ scripts/run_stt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sleep $2
 cd $2/src/speech/speech_to_text/
-export $2/GOOGLE_APPLICATION_CREDENTIALS=creds.json
+export GOOGLE_APPLICATION_CREDENTIALS=$2/creds.json
 python gspeech_live.py
