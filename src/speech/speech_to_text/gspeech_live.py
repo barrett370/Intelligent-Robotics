ff700b6fd2812a6f488c6005d715e903e0971a3b
Sam <barrett370@gmail.com>
2019-12-05 15:41:44 +0000

    change mic method

--- src/speech/speech_to_text/gspeech_live.py
+++ src/speech/speech_to_text/gspeech_live.py
@@ -29,7 +29,7 @@ def get_current_time():
 class ResumableMicrophoneStream:
     """Opens a recording stream as a generator yielding the audio chunks."""
 
-    def __init__(self, rate, chunk_size):
+    def __init__(self, rate, chunk_size, mic_id=None):
         self._rate = rate
         self.chunk_size = chunk_size
         self._num_channels = 1
@@ -46,25 +46,39 @@ class ResumableMicrophoneStream:
         self.last_transcript_was_final = False
         self.new_stream = True
         self._audio_interface = pyaudio.PyAudio()
-        self._audio_stream = self._audio_interface.open(
-            format=pyaudio.paInt16,
-            channels=self._num_channels,
-            rate=self._rate,
-            input=True,
-            # input_device_index=0,
-            frames_per_buffer=self.chunk_size,
-            # Run the audio stream asynchronously to fill the buffer object.
-            # This is necessary so that the input device's buffer doesn't
-            # overflow while the calling thread makes network requests, etc.
-            stream_callback=self._fill_buffer,
-        )
+        if mic_id is None:
+            self._audio_stream = self._audio_interface.open(
+                format=pyaudio.paInt16,
+                channels=self._num_channels,
+                rate=self._rate,
+                input=True,
+                # input_device_index=mic_id,
+                frames_per_buffer=self.chunk_size,
+                # Run the audio stream asynchronously to fill the buffer object.
+                # This is necessary so that the input device's buffer doesn't
+                # overflow while the calling thread makes network requests, etc.
+                stream_callback=self._fill_buffer,
+            )
+        else:
+            self._audio_stream = self._audio_interface.open(
+                format=pyaudio.paInt16,
+                channels=self._num_channels,
+                rate=self._rate,
+                input=True,
+                input_device_index=mic_id,
+                frames_per_buffer=self.chunk_size,
+                # Run the audio stream asynchronously to fill the buffer object.
+                # This is necessary so that the input device's buffer doesn't
+                # overflow while the calling thread makes network requests, etc.
+                stream_callback=self._fill_buffer,
+            )
 
     def __enter__(self):
 
         self.closed = False
         return self
 
-    def __exit__(self, type, value, traceback):
+    def __exit__(self):
 
         self._audio_stream.stop_stream()
         self._audio_stream.close()
@@ -135,109 +149,115 @@ class ResumableMicrophoneStream:
             yield b''.join(data)
 
 
-listening_end = 0
-parser = InstructionParser()
-continued = False
+class Listener:
 
-def parse_input_stream(responses):
-    global listening_end
-    global continued
-    print("attempting to parse input")
-    if responses: 
-        for response in responses:
-            print(1)
-            if not response.results:
-                break
+    def __init__(self):
+        self.listening_end = 0
+        self.parser = InstructionParser()
+        self.continued = False
+        self.mic_manager = ResumableMicrophoneStream(SAMPLE_RATE, CHUNK_SIZE)
 
-            result = response.results[0]
-            if not result.alternatives:
-                continue
-            if result.is_final:
-                seq = 0 
-                transcript: str = result.alternatives[0].transcript
-                seq = difflib.SequenceMatcher(a= "howard", b=  transcript.lower()).ratio()
-                # if seq > sim:
-                #     sim = seq 
-                # print(2)
-                print(f"Sim is : {seq}")
-                # if transcript.__contains__("Howard"):
-                if seq > 0.7:
-                    # trigger motion and vision to scan for speaker (first face detected)
-                    sys.stdout.write(RED)
-                    sys.stdout.write("Wake Word Detected\n")
-                    os.system("mpg321 ../resources/activation.mp3")
-                    listening_end = get_current_time() + 1000000
-                elif listening_end > get_current_time():
-                    transcript = strip_leading_space(transcript)
-                    print(f"checking for commands {transcript}")
-                    # instructions[transcript.lower()]()
-                    if parser.parse(transcript.lower(),continued):
-                        print("completed instruction waiting for wake word")
-                        listening_end = get_current_time()
-                        continued = False
-                    else:
-                        print("Next instruction will be a response")
-                        continued = True
-    print("returning")
-    return
+    def change_mic_id(self, mic_id: int):
+        self.mic_manager.__exit__()
+        self.mic_manager = ResumableMicrophoneStream(SAMPLE_RATE, CHUNK_SIZE, mic_id=mic_id)
+        self.main()
+
+    def parse_input_stream(self, responses):
+
+        print("attempting to parse input")
+        if responses:
+            for response in responses:
+                print(1)
+                if not response.results:
+                    break
+
+                result = response.results[0]
+                if not result.alternatives:
+                    continue
+                if result.is_final:
+                    transcript: str = result.alternatives[0].transcript
+                    seq = difflib.SequenceMatcher(a="howard", b=transcript.lower()).ratio()
+                    # if seq > sim:
+                    #     sim = seq
+                    # print(2)
+                    print(f"Sim is : {seq}")
+                    # if transcript.__contains__("Howard"):
+                    if seq > 0.7:
+                        # trigger motion and vision to scan for speaker (first face detected)
+                        sys.stdout.write(RED)
+                        sys.stdout.write("Wake Word Detected\n")
+                        os.system("mpg321 ../resources/activation.mp3")
+                        self.listening_end = get_current_time() + 1000000
+                    elif self.listening_end > get_current_time():
+                        transcript = strip_leading_space(transcript)
+                        print(f"checking for commands {transcript}")
+                        # instructions[transcript.lower()]()
+                        if self.parser.parse(transcript.lower(), self.continued):
+                            print("completed instruction waiting for wake word")
+                            self.listening_end = get_current_time()
+                            self.continued = False
+                        else:
+                            print("Next instruction will be a response")
+                            self.continued = True
+        print("returning")
+        return
 
         # Parse following reponses for commands
 
+    def main(self):
+        """start bidirectional streaming from microphone input to speech API"""
 
-def main():
-    """start bidirectional streaming from microphone input to speech API"""
+        client = speech.SpeechClient()
+        config = speech.types.RecognitionConfig(
+            encoding=speech.enums.RecognitionConfig.AudioEncoding.LINEAR16,
+            sample_rate_hertz=SAMPLE_RATE,
+            language_code='en-US',
+            max_alternatives=1)
+        streaming_config = speech.types.StreamingRecognitionConfig(
+            config=config,
+            interim_results=False)
 
-    client = speech.SpeechClient()
-    config = speech.types.RecognitionConfig(
-        encoding=speech.enums.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=SAMPLE_RATE,
-        language_code='en-US',
-        max_alternatives=1)
-    streaming_config = speech.types.StreamingRecognitionConfig(
-        config=config,
-        interim_results=False)
+        print(self.mic_manager.chunk_size)
+        sys.stdout.write(YELLOW)
+        sys.stdout.write('End (ms)       Transcript Results/Status\n')
+        sys.stdout.write('=====================================================\n')
 
-    mic_manager = ResumableMicrophoneStream(SAMPLE_RATE, CHUNK_SIZE)
-    print(mic_manager.chunk_size)
-    sys.stdout.write(YELLOW)
-    sys.stdout.write('End (ms)       Transcript Results/Status\n')
-    sys.stdout.write('=====================================================\n')
+        with self.mic_manager as stream:
 
-    with mic_manager as stream:
+            while not stream.closed:
+                sys.stdout.write(YELLOW)
+                sys.stdout.write('\n' + str(
+                    STREAMING_LIMIT * stream.restart_counter) + ': NEW REQUEST\n')
 
-        while not stream.closed:
-            sys.stdout.write(YELLOW)
-            sys.stdout.write('\n' + str(
-                STREAMING_LIMIT * stream.restart_counter) + ': NEW REQUEST\n')
+                stream.audio_input = []
+                audio_generator = stream.generator()
 
-            stream.audio_input = []
-            audio_generator = stream.generator()
+                requests = (speech.types.StreamingRecognizeRequest(
+                    audio_content=content) for content in audio_generator)
 
-            requests = (speech.types.StreamingRecognizeRequest(
-                audio_content=content) for content in audio_generator)
+                responses = client.streaming_recognize(streaming_config, requests)
+                # Now, put the transcription responses to use.
+                try:
+                    print("Parsing input")
+                    self.parse_input_stream(responses)
+                except google.api_core.exceptions.DeadlineExceeded:
+                    # print("cannot parse")
+                    continue
+                # listen_print_loop(responses, stream)
 
-            responses = client.streaming_recognize(streaming_config,requests)
-            # Now, put the transcription responses to use.
-            try:
-                print("Parsing input")
-                parse_input_stream(responses)
-            except google.api_core.exceptions.DeadlineExceeded:
-                # print("cannot parse")
-                continue
-            # listen_print_loop(responses, stream)
+                if stream.result_end_time > 0:
+                    stream.final_request_end_time = stream.is_final_end_time
+                stream.result_end_time = 0
+                stream.last_audio_input = []
+                stream.last_audio_input = stream.audio_input
+                stream.audio_input = []
+                stream.restart_counter = stream.restart_counter + 1
 
-            if stream.result_end_time > 0:
-                stream.final_request_end_time = stream.is_final_end_time
-            stream.result_end_time = 0
-            stream.last_audio_input = []
-            stream.last_audio_input = stream.audio_input
-            stream.audio_input = []
-            stream.restart_counter = stream.restart_counter + 1
-
-            if not stream.last_transcript_was_final:
-                sys.stdout.write('\n')
-            stream.new_stream = True
+                if not stream.last_transcript_was_final:
+                    sys.stdout.write('\n')
+                stream.new_stream = True
 
 
 if __name__ == '__main__':
-    main()
+    listener = Listener()
+    listener.main()
